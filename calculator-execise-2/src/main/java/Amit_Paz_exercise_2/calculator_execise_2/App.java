796e3aa0653326445dd6f427ad6fb5026ef2696f
amit1 <amit1@Amit-PC.lan>
2024-01-16 15:36:51 +0200

    commit 2 - function 'numberToChar' added and tests for 'F' grade

--- calculator-execise-2/src/main/java/Amit_Paz_exercise_2/calculator_execise_2/App.java
+++ calculator-execise-2/src/main/java/Amit_Paz_exercise_2/calculator_execise_2/App.java
@@ -6,8 +6,32 @@ package Amit_Paz_exercise_2.calculator_execise_2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static String numberToChar(int number)
+	{
+		if(number < 1)
+			return "Error";
+		
+		else if( 1 <= number && number <= 49 )
+			return "F";
+		
+		else if( 50 <= number && number <= 59 )
+			return "E";
+		
+		else if( 60 <= number && number <= 69 )
+			return "D";
+		
+		else if( 70 <= number && number <= 79 )
+			return "C";
+		
+		else if( 80 <= number && number <= 89 )
+			return "B";
+		
+		else if( 90 <= number && number <= 100 )
+			return "A";
+		
+		else if( 100 < number )
+			return "Error";
+		
+		return "Error"; // it won't get here
+	}
 }
--- calculator-execise-2/src/test/java/Amit_Paz_exercise_2/calculator_execise_2/AppTest.java
+++ calculator-execise-2/src/test/java/Amit_Paz_exercise_2/calculator_execise_2/AppTest.java
@@ -1,5 +1,6 @@
 package Amit_Paz_exercise_2.calculator_execise_2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,9 +13,44 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+	
+	// Start F check
     @Test
-    public void shouldAnswerWithTrue()
+    public void ex2_partition_F_min()
     {
-        assertTrue( true );
+    	int number = 1;
+        String expectedResult = "F";
+        assertEquals(expectedResult, App.numberToChar(number));       
     }
+    @Test
+    public void ex2_partition_F_minPlus1()
+    {
+    	int number = 2;
+        String expectedResult = "F";
+        assertEquals(expectedResult, App.numberToChar(number)); 
+    }
+    @Test
+    public void ex2_partition_F_nominal()
+    {
+    	int number = 25;
+        String expectedResult = "F";
+        assertEquals(expectedResult, App.numberToChar(number)); 
+    }
+    @Test
+    public void ex2_partition_F_maxMinus1()
+    {
+    	int number = 48;
+        String expectedResult = "F";
+        assertEquals(expectedResult, App.numberToChar(number)); 
+    }
+    @Test
+    public void ex2_partition_F_max()
+    {
+    	int number = 49;
+        String expectedResult = "F";
+        assertEquals(expectedResult, App.numberToChar(number)); 
+    }
+    // End F check
+      
+    
 }
