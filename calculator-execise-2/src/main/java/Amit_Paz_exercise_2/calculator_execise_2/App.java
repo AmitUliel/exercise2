9a360002ac9d51efe69da7b67089b57544800b8b
amit1 <amit1@Amit-PC.lan>
2024-01-16 14:07:29 +0200

    first commit (checking).

---
+++ calculator-execise-2/src/main/java/Amit_Paz_exercise_2/calculator_execise_2/App.java
@@ -0,0 +1,13 @@
+package Amit_Paz_exercise_2.calculator_execise_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ calculator-execise-2/src/test/java/Amit_Paz_exercise_2/calculator_execise_2/AppTest.java
@@ -0,0 +1,20 @@
+package Amit_Paz_exercise_2.calculator_execise_2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
