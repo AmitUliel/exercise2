a1cc7f19f38169c4a726f021ec92cbbadd7d8756
amit1 <amit1@Amit-PC.lan>
2024-01-21 14:06:49 +0200

    commit 3 - changed all the conditionals to from 'else if' to 'if'

--- calculator-execise-2/src/main/java/Amit_Paz_exercise_2/calculator_execise_2/App.java
+++ calculator-execise-2/src/main/java/Amit_Paz_exercise_2/calculator_execise_2/App.java
@@ -11,25 +11,25 @@ public class App
 		if(number < 1)
 			return "Error";
 		
-		else if( 1 <= number && number <= 49 )
+		if( 1 <= number && number <= 49 )
 			return "F";
 		
-		else if( 50 <= number && number <= 59 )
+		if( 50 <= number && number <= 59 )
 			return "E";
 		
-		else if( 60 <= number && number <= 69 )
+		if( 60 <= number && number <= 69 )
 			return "D";
 		
-		else if( 70 <= number && number <= 79 )
+		if( 70 <= number && number <= 79 )
 			return "C";
 		
-		else if( 80 <= number && number <= 89 )
+		if( 80 <= number && number <= 89 )
 			return "B";
 		
-		else if( 90 <= number && number <= 100 )
+		if( 90 <= number && number <= 100 )
 			return "A";
 		
-		else if( 100 < number )
+		if( 100 < number )
 			return "Error";
 		
 		return "Error"; // it won't get here
